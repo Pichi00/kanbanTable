b1bd1fea5c66b7ebaae527152ee8729cc5b1e9ff
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2022-12-28 16:20:04 +0100

    feat: remove dripsy

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,28 +1,14 @@
-import { makeTheme, DripsyProvider, H1 } from "dripsy";
 import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
-const theme = makeTheme({});
+import { Text } from "react-native";
 
 const App = () => {
   return (
-    <DripsyProvider theme={theme}>
-      <View style={styles.container}>
-        <Text>Dzbanban</Text>
-        <StatusBar style="auto" />
-      </View>
-    </DripsyProvider>
+    <>
+      <Text>Dzbanban</Text>
+      <StatusBar style="auto" />
+    </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 registerRootComponent(App);
