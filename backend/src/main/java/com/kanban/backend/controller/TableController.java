8e4955ed6244c7cf22616a597e60297a3caf41eb
Kacper Grabiec <grabiec.kacper01@gmail.com>
2023-01-11 18:02:35 +0100

    add pdf endpoint

--- backend/src/main/java/com/kanban/backend/controller/TableController.java
+++ backend/src/main/java/com/kanban/backend/controller/TableController.java
@@ -1,12 +1,18 @@
 package com.kanban.backend.controller;
 
+import com.kanban.backend.generator.PDFGenerator;
 import com.kanban.backend.model.Table;
 import com.kanban.backend.model.User;
 import com.kanban.backend.service.TableService;
 import com.kanban.backend.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -14,6 +20,24 @@ import java.util.List;
 public class TableController {
     private final TableService tableService;
     private final UserService userService;
+
+    @GetMapping("/pdf/{id}")
+    public void generatePDF(@PathVariable Long id, HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd:HH:mm:ss");
+        String currentDateTime = dateFormat.format(new Date());
+
+        String headerkey = "Content-Disposition";
+        String headervalue = "attachment; filename=table" + id + currentDateTime + ".pdf";
+
+        response.setHeader(headerkey, headervalue);
+
+        Table table = this.tableService.getTableById(id);
+
+        PDFGenerator pdfGenerator = new PDFGenerator();
+        pdfGenerator.generate(table, response);
+    }
 
     @GetMapping("/tables")
     public List<Table> getAllTables() {
