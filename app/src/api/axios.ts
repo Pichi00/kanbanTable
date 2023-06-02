710d2e8dfbffbfe08796050bcd698d51eec1b2c5
Kacper Grabiec <grabiec.kacper01@gmail.com>
2023-06-02 19:27:23 +0200

    prevents adding same tag to task multiple times

--- app/src/api/axios.ts
+++ app/src/api/axios.ts
@@ -3,7 +3,7 @@ import * as SecureStore from "expo-secure-store";
 import { API_URL, API_PORT } from "@env";
 
 const apiClient = axios.create({
-  baseURL: `http://104.248.45.230:8080`,
+  baseURL: `http://localhost:8080`,
   headers: {
     "Content-Type": "application/json",
   },
